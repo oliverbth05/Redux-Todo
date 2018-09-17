b506a3f38a483c433e4626c820a4fe9139f68888
Oliver Bath <oliverbth05@gmail.com>
2018-09-17 20:47:05 +0000

    Responsive CSS

--- src/components/Task.js
+++ src/components/Task.js
@@ -7,7 +7,7 @@ const Task = (props) => {
         <div className = {props.style} onClick = {() => {props.selectTask(props.index)}}  >
             <button onClick = {()=> {props.completeTask(props.index)}} class = 'button button-sm'  ><i class="fas fa-check-circle"></i> Complete</button>
             <p className = 'task-title'>{props.title}</p>
-            <p className = 'task-date'><i class="far fa-clock"></i> {props.daysLeft || props.hoursLeft } {props.daysLeft ? 'Days left' : 'Hours left'}</p>
+            <p className = 'task-date'><i class="far fa-clock"></i> {props.daysLeft || props.hoursLeft } {props.daysLeft ? 'Days' : 'Hours'}</p>
         </div>
         )
 }
